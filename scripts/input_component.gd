a63063066abf57f04f8a653c1d509a93ca0d0d5a
Sergej Luzhin <luzhins764@gmail.com>
2026-06-17 00:36:08 +0300

    changed movement component

--- scripts/input_component.gd
+++ scripts/input_component.gd
@@ -4,6 +4,7 @@ var move_dir: Vector2 = Vector2.ZERO
 signal jump_pressed
 signal heal_pressed
 signal hurt_pressed
+signal move_input_changed(input_vector: Vector2)
 
 func update() -> void:
 	move_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -13,4 +14,22 @@ func update() -> void:
 		heal_pressed.emit()
 	if Input.is_action_just_pressed("hurt"):
 		hurt_pressed.emit()
+		
+
+
+func _process(_delta: float) -> void:
+	# Читаем движение по горизонтали и вертикали
+	var horizontal := Input.get_axis("move_left", "move_right")
+	var vertical := Input.get_axis("move_up", "move_down")
+	var input_vector := Vector2(horizontal, vertical)
+	
+	# Нормализуем для диагонали
+	if input_vector.length() > 1.0:
+		input_vector = input_vector.normalized()
+		
+	move_input_changed.emit(input_vector)
+	
+	# Прыжок
+	if Input.is_action_just_pressed("jump"):
+		jump_pressed.emit()
 	
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,21 +6,29 @@ class_name Player extends CharacterBody2D
 
 func _ready() -> void:
 	if input_component: 
+		input_component.move_input_changed.connect(_on_move_input_changed)
 		input_component.jump_pressed.connect(_on_jump_pressed)
 		input_component.heal_pressed.connect(_on_heal_pressed)
 		input_component.hurt_pressed.connect(_on_hurt_pressed)
 	if health_component:
 		health_component.has_died.connect(_on_has_died)
+		
+# --- Обработчики сигналов (Call Down) ---
+
+func _on_move_input_changed(input_vector: Vector2):
+	# Передаём управление движением в MovementComponent
+	movement_component.move(input_vector, get_process_delta_time())
 
 
 func _physics_process(delta: float) -> void:
+	move_and_slide()
 	
 	# Read controls
-	input_component.update()
+	#input_component.update()
 	
 	# Read Movement Component
-	movement_component.direction = input_component.move_dir
-	movement_component.tick(delta)
+	#movement_component.direction = input_component.move_dir
+	#movement_component.tick(delta)
 			
 func _on_jump_pressed() -> void:
 	pass
